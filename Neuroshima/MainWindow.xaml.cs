a265a31f6a3011b09dd1e2ebb08a6c6a62ceebee
Michal Kozik <michal.kozikowski99@gmail.com>
2021-03-24 13:04:27 +0100

    Tworzenie dowolnych rozmiarów planszy.

--- Neuroshima/MainWindow.xaml.cs
+++ Neuroshima/MainWindow.xaml.cs
@@ -25,65 +25,86 @@ namespace Neuroshima
         public MainWindow()
         {
             InitializeComponent();
-            CreateBoard();
+            //CreateBoard();
         }
 
-        private void CreateBoard()
+        private void CreateBoard(object sender, RoutedEventArgs e)
         {
-            GameboardCreator creator = new GameboardCreator();
-            for (int i = 0; i < 26; i++)
+            //GameboardCreator creator = new GameboardCreator();
+            int rows, cols;
+            bool rowsLoaded = Int32.TryParse(InputRows.Text, out rows);
+            bool colsLoaded = Int32.TryParse(InputCols.Text, out cols);
+
+            if (rowsLoaded && colsLoaded)
             {
-                for (int j = 0; j < 26; j++)
+                if (rows > 0 && rows < 26 && cols > 0 && cols < 26)
                 {
-                    if (j == 0 && i != 0)
+                    for (int i = 0; i < rows; i++)
                     {
-                        Border border = new Border()
+                        for (int j = 0; j < cols; j++)
                         {
-                            Width = 25,
-                            Height = 25,
-                            Child = new TextBlock() { Text = $"Y{i}", VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center },
-                        };
-                        GameBoard.Children.Add(border);
-                        Canvas.SetTop(border, i * 25);
-                        Canvas.SetLeft(border, j * 25);
-                    }
-                    else if (i == 0 && j != 0)
-                    {
-                        Border border = new Border()
-                        {
-                            Width = 25,
-                            Height = 25,
-                            Child = new TextBlock() { Text = $"X{j}", VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center },
-                        };
-                        GameBoard.Children.Add(border);
-                        Canvas.SetTop(border, i * 25);
-                        Canvas.SetLeft(border, j * 25);
-                    }
-                    else
-                    {
-                        if (i == 0 && j == 0)
-                            continue;
-                        Rectangle rec = new Rectangle()
-                        {
-                            Name = $"R_{i}_{j}",
-                            Width = 25,
-                            Height = 25,
-                            //Fill = Brushes.Gray,
-                            Fill = null,
-                            Stroke = Brushes.Black,
-                            StrokeThickness = 1,
-                        };
-                        rec.MouseEnter += new MouseEventHandler(MouseEnterField);
-                        rec.MouseLeave += new MouseEventHandler(MouseLeftField);
-                        rec.MouseDown += new MouseButtonEventHandler(ClickedDownFiled);
-                        rec.MouseUp += new MouseButtonEventHandler(ClickedUpField);
-                        GameBoard.Children.Add(rec);
-                        Canvas.SetTop(rec, i * 25);
-                        Canvas.SetLeft(rec, j * 25);
-                        Point point = new Point(j, i);
-                        creator.AddRectangle(point, rec);
+                            // Oznaczenie pionowe.
+                            if (j == 0 && i != 0)
+                            {
+                                Border border = new Border()
+                                {
+                                    Width = 25,
+                                    Height = 25,
+                                    Child = new TextBlock() { Text = $"Y{i}", VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center },
+                                };
+                                GameBoard.Children.Add(border);
+                                Canvas.SetTop(border, i * 25);
+                                Canvas.SetLeft(border, j * 25);
+                            }
+                            // Oznaczenie poziome.
+                            else if (i == 0 && j != 0)
+                            {
+                                Border border = new Border()
+                                {
+                                    Width = 25,
+                                    Height = 25,
+                                    Child = new TextBlock() { Text = $"X{j}", VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center },
+                                };
+                                GameBoard.Children.Add(border);
+                                Canvas.SetTop(border, i * 25);
+                                Canvas.SetLeft(border, j * 25);
+                            }
+                            // Pola do interakcji.
+                            else
+                            {
+                                if (i == 0 && j == 0)
+                                    continue;
+                                Rectangle rec = new Rectangle()
+                                {
+                                    Name = $"R_{i}_{j}",
+                                    Width = 25,
+                                    Height = 25,
+                                    //Fill = Brushes.Gray,
+                                    Fill = null,
+                                    Stroke = Brushes.Black,
+                                    StrokeThickness = 1,
+                                };
+                                rec.MouseEnter += new MouseEventHandler(MouseEnterField);
+                                rec.MouseLeave += new MouseEventHandler(MouseLeftField);
+                                rec.MouseDown += new MouseButtonEventHandler(ClickedDownFiled);
+                                rec.MouseUp += new MouseButtonEventHandler(ClickedUpField);
+                                GameBoard.Children.Add(rec);
+                                Canvas.SetTop(rec, i * 25);
+                                Canvas.SetLeft(rec, j * 25);
+                                //Point point = new Point(j, i);
+                                //creator.AddRectangle(point, rec);
+                            }
+                        }
                     }
                 }
+                else
+                {
+                    MessageBox.Show($"Wartości powinny być z zakresu od 1 do 25.", "Niepoprawny przedział", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Niepoprawne wartości!", "Niepoprawne wartości", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
